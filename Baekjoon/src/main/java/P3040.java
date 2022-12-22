5a41e8cf924ccb8ffdf12d035cca6590b09b0910
dhrudgns529 <dhrudgns529@naver.com>
2022-12-22 17:11:10 +0900

    22.12.22

---
+++ Baekjoon/src/main/java/P3040.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class P3040 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int[] arr = new int[9];
+        for(int i=0; i<9; i++){
+            arr[i] = in.nextInt();
+        }
+        int sum = -100;
+
+        for(int i=0; i<9; i++){
+            sum += arr[i];
+        }
+
+        int notNonJaeng1 = 0;
+        int notNonJaeng2 = 0;
+
+        for(int i=0; i<8; i++){
+            for(int j=i+1; j<9; j++){
+                if (arr[i] + arr[j] == sum){
+                    notNonJaeng1 = arr[i];
+                    notNonJaeng2 = arr[j];
+                }
+            }
+        }
+
+        for(int i=0; i<9; i++){
+            if(arr[i] != notNonJaeng1 && arr[i] != notNonJaeng2) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
